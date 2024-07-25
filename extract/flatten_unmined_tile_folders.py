935993ba85cbbf74deccca500e8ab6ef2f00f447
zachaa <57045291+zachaa@users.noreply.github.com>
2024-07-24 22:54:57 -0500

    more paths for different maps, add zoom 1 and 2

--- extract/flatten_unmined_tile_folders.py
+++ extract/flatten_unmined_tile_folders.py
@@ -2,6 +2,8 @@ from pathlib import Path
 import shutil
 
 top_level_paths = [
+    Path(r"C:\Users\Zachary\Coding\VSCode\DrehmalMap\src\images\maps\overworld\tiles\zoom.2"),
+    Path(r"C:\Users\Zachary\Coding\VSCode\DrehmalMap\src\images\maps\overworld\tiles\zoom.1"),
     Path(r"C:\Users\Zachary\Coding\VSCode\DrehmalMap\src\images\maps\overworld\tiles\zoom.0"),
     Path(r"C:\Users\Zachary\Coding\VSCode\DrehmalMap\src\images\maps\overworld\tiles\zoom.-1"),
     Path(r"C:\Users\Zachary\Coding\VSCode\DrehmalMap\src\images\maps\overworld\tiles\zoom.-2"),
@@ -9,6 +11,27 @@ top_level_paths = [
     Path(r"C:\Users\Zachary\Coding\VSCode\DrehmalMap\src\images\maps\overworld\tiles\zoom.-4"),
     Path(r"C:\Users\Zachary\Coding\VSCode\DrehmalMap\src\images\maps\overworld\tiles\zoom.-5"),
     Path(r"C:\Users\Zachary\Coding\VSCode\DrehmalMap\src\images\maps\overworld\tiles\zoom.-6")
+]
+
+top_level_paths_end = [
+    Path(r"C:\Users\Zachary\Coding\VSCode\DrehmalMap\src\images\maps\end\tiles\zoom.2"),
+    Path(r"C:\Users\Zachary\Coding\VSCode\DrehmalMap\src\images\maps\end\tiles\zoom.1"),
+    Path(r"C:\Users\Zachary\Coding\VSCode\DrehmalMap\src\images\maps\end\tiles\zoom.0"),
+    Path(r"C:\Users\Zachary\Coding\VSCode\DrehmalMap\src\images\maps\end\tiles\zoom.-1"),
+    Path(r"C:\Users\Zachary\Coding\VSCode\DrehmalMap\src\images\maps\end\tiles\zoom.-2"),
+    Path(r"C:\Users\Zachary\Coding\VSCode\DrehmalMap\src\images\maps\end\tiles\zoom.-3")
+]
+
+top_level_paths_true_end = [
+    Path(r"C:\Users\Zachary\Coding\VSCode\DrehmalMap\src\images\maps\true_end\tiles\zoom.2"),
+    Path(r"C:\Users\Zachary\Coding\VSCode\DrehmalMap\src\images\maps\true_end\tiles\zoom.1"),
+    Path(r"C:\Users\Zachary\Coding\VSCode\DrehmalMap\src\images\maps\true_end\tiles\zoom.0"),
+    Path(r"C:\Users\Zachary\Coding\VSCode\DrehmalMap\src\images\maps\true_end\tiles\zoom.-1"),
+    Path(r"C:\Users\Zachary\Coding\VSCode\DrehmalMap\src\images\maps\true_end\tiles\zoom.-2"),
+    Path(r"C:\Users\Zachary\Coding\VSCode\DrehmalMap\src\images\maps\true_end\tiles\zoom.-3"),
+    Path(r"C:\Users\Zachary\Coding\VSCode\DrehmalMap\src\images\maps\true_end\tiles\zoom.-4"),
+    Path(r"C:\Users\Zachary\Coding\VSCode\DrehmalMap\src\images\maps\true_end\tiles\zoom.-5"),
+    Path(r"C:\Users\Zachary\Coding\VSCode\DrehmalMap\src\images\maps\true_end\tiles\zoom.-6")
 ]
 
 def move_png_files(top_level_dir: Path):
@@ -19,10 +42,10 @@ def move_png_files(top_level_dir: Path):
             shutil.move(str(path), str(destination))
     print(f"{top_level_dir.name} finished")
 
-def move_all_png_files():
-    for top_dir in top_level_paths:
+def move_all_png_files(path_list):
+    for top_dir in path_list:
         move_png_files(top_dir)
     print("Remember to delete folders and metadata.")
 
 if __name__ == "__main__":
-    move_all_png_files()
+    move_all_png_files(top_level_paths_true_end)
