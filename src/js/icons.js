cc586c209cda62b870c187f36560d5038ecb2df6
zachaa <57045291+zachaa@users.noreply.github.com>
2024-07-31 23:05:21 -0500

    Fix icon popup location

--- src/js/icons.js
+++ src/js/icons.js
@@ -2,7 +2,7 @@ var Icon64 = L.Icon.extend({
     options: {
         iconSize:     [64, 64],
         iconAnchor:   [32, 32],
-        popupAnchor:  [32, -1]
+        popupAnchor:  [0, -32]
     }
 });
 
@@ -10,7 +10,7 @@ var Icon32 = L.Icon.extend({
     options: {
         iconSize:     [32, 32],
         iconAnchor:   [16, 16],
-        popupAnchor:  [16, -1]
+        popupAnchor:  [0, -16]
     }
 });
 
@@ -18,7 +18,7 @@ var Icon16 = L.Icon.extend({
     options: {
         iconSize:     [16, 16],
         iconAnchor:   [8, 8],
-        popupAnchor:  [8, -1]
+        popupAnchor:  [0, -8]
     }
 });
 
@@ -26,12 +26,12 @@ let TowerIcon = new L.Icon({
         iconUrl: "images/icons/custom/Tower.png",
         iconSize:     [16, 16],
         iconAnchor:   [8, 8],
-        popupAnchor:  [8, -1]
+        popupAnchor:  [0, -8]
 });
 
 let LodahrPortalIcon = new L.Icon({
         iconUrl: "images/icons/custom/purpur_pillar_top.png",
         iconSize:     [16, 16],
         iconAnchor:   [8, 8],
-        popupAnchor:  [8, -1]
+        popupAnchor:  [0, -8]
 });
